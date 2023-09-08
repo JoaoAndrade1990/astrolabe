b9b17ccbcec0ee9c7d626fa2d2bea681d28c8345
JoaoAndrade1990 <joaomagalhaesandrade@gmail.com>
2023-09-08 10:18:01 +0100

    header update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,63 +1,62 @@
-import { Navbar, Nav, Form, FormControl, InputGroup } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import CartContext from '../contexts/CartContext';
-import {useContext} from 'react'
+import {
+  Navbar,
+  Nav,
+  Form,
+  FormControl,
+  InputGroup,
+  Container,
+} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import CartContext from "../contexts/CartContext";
+import { useContext } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const Header = () => {
   const { cart } = useContext(CartContext);
 
   return (
-    <Navbar expand='lg' className='border-bottom'>
-      <div className='container '>
-        <Navbar.Brand as={Link} to='/'>
-          <img src='/src/assets/logo.svg' alt='Logo' />
+    <Navbar expand="lg" className="border-bottom">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          <img src="/src/assets/logo.svg" alt="Logo" />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-
-        <Navbar.Collapse
-          id='basic-navbar-nav'
-          className='d-flex justify-content-between'
-        >
-          <Nav>
-            <Nav.Link as={Link} to='/products'>
+        <Form className="d-flex">
+          <InputGroup>
+            <InputGroup.Text>
+              <i className="bi bi-search"></i>
+            </InputGroup.Text>
+            <FormControl
+              type="text"
+              placeholder="Search"
+              className="border-1 border-bottom mr-2"
+            />
+          </InputGroup>
+        </Form>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/products">
               Shop
             </Nav.Link>
-            
-            <Nav.Link as={Link} to='/users'>
+            <Nav.Link as={Link} to="/users">
               Users
             </Nav.Link>
           </Nav>
-
-          <Form className='d-flex'>
-            <InputGroup>
-              <InputGroup.Text>
-                <i className='bi bi-search'></i>
-              </InputGroup.Text>
-              <FormControl
-                type='text'
-                placeholder='Search'
-                className='border-1 border-bottom mr-2'
-              />
-            </InputGroup>
-          </Form>
-
           <Nav>
-            <Nav.Link as={Link} to='/cart'>
-              <i className='bi bi-cart'></i>
-              <span style={{ color: 'black', marginLeft: '6px' }}>
+            <Nav.Link as={Link} to="/cart">
+              <i className="bi bi-cart"></i>
+              <span style={{ color: "black", marginLeft: "6px" }}>
                 {cart.length !== 0 ? cart.length : null}
               </span>
             </Nav.Link>
-            <Nav.Link as={Link} to='/login'>
+            <Nav.Link as={Link} to="/login">
               Login
             </Nav.Link>
           </Nav>
-
         </Navbar.Collapse>
-      </div>
+      </Container>
     </Navbar>
   );
 };
 
 export default Header;
-
