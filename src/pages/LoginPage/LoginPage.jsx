233393681164d04e870b485396f80ab264d9ffee
JoaoAndrade1990 <joaomagalhaesandrade@gmail.com>
2023-09-12 17:20:25 +0100

    update on the loginpage

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,10 +1,37 @@
-import { useState, useEffect } from "react";
-import { Container, Row, Col, Button } from "react-bootstrap";
-import Loading from "../../components/Loading/Loading";
-import Form from "react-bootstrap/Form";
+import { useState } from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import Loading from '../../components/Loading/Loading';
+import Form from 'react-bootstrap/Form';
 
 function LoginPage() {
   const [loading, setLoading] = useState(false);
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const [loginSuccess, setLoginSuccess] = useState(false);
+  const [error, setError] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    if (!email || !password) {
+      setError('Campos obrigatórios');
+      return;
+    }
+
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      if (email === 'email@gmail.com' && password === '123456') {
+        setLoginSuccess(true);
+        setError('');
+      } else {
+        setLoginSuccess(false);
+        setError('Credenciais inválidas. Tente novamente.');
+      }
+    }, 500);
+  };
 
   return (
     <Container>
@@ -12,46 +39,57 @@ function LoginPage() {
         <Loading />
       ) : (
         <>
-        <br />
-        <br />
-        <Row> 
-        <div className="mx-auto col-10 col-md-8 col-lg-6">
-          <Form> 
-            <h3 className="text-center">Welcome Back</h3>
-            <br />
-            <div className="form-group">
-              <label htmlFor="email">Email address</label>
-              <input
-                type="email"
-                className="form-control"
-                id="email"
-                aria-describedby="email"
-                placeholder="Enter email"
-              />
-              <small id="emailHelp" className="form-text text-muted">
-                We'll never share your email with anyone else.
-              </small>
+          <br />
+          <br />
+          <Row>
+            <div className='mx-auto col-10 col-md-8 col-lg-6'>
+              <Form onSubmit={handleSubmit}>
+                <h3 className='text-center'>Welcome Back</h3>
+                <br />
+                <div className={`form-group ${error ? 'has-error' : ''}`}>
+                  <label htmlFor='email'>Email address</label>
+                  <input
+                    type='email'
+                    className='form-control'
+                    id='email'
+                    aria-describedby='email'
+                    placeholder='Enter email'
+                  />
+                  <small id='emailHelp' className='form-text text-muted'>
+                    We'll never share your email with anyone else.
+                  </small>
+                </div>
+                <br />
+                <div className={`form-group ${error ? 'has-error' : ''}`}>
+                  <label htmlFor='password'>Password</label>
+                  <input
+                    type='password'
+                    className='form-control'
+                    id='password'
+                    placeholder='Enter Password'
+                  />
+                </div>
+                <br />
+                {error && <div className='alert alert-danger'>{error}</div>}
+                {formSubmitted && loginSuccess && (
+                  <div className='alert alert-success'>
+                    Login realizado com sucesso!
+                  </div>
+                )}
+                <Row>
+                  <Col>
+                    <Button
+                      type='submit'
+                      className='btn btn-primary btn-lg btn-block w-100'
+                      variant='dark'
+                      onClick={() => setFormSubmitted(true)}
+                    >
+                      Submit
+                    </Button>
+                  </Col>
+                </Row>
+              </Form>
             </div>
-            <br />
-            <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                className="form-control"
-                id="password"
-                placeholder="Enter Password"
-              />
-            </div>
-            <br />
-            <Row>
-                <Col> 
-                <Button type="submit"  className="btn btn-primary btn-lg btn-block w-100" 
-            variant='dark'>Submit</Button>
-                </Col>
-            </Row>
-           
-          </Form>
-          </div>
           </Row>
           <br />
           <br />
@@ -66,7 +104,6 @@ function LoginPage() {
           <br />
           <br />
           <br />
-
         </>
       )}
     </Container>
