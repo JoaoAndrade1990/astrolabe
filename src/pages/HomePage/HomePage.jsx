6c8fcaaaab14d08fd3b421bd603db581f13500f6
JoaoAndrade1990 <joaomagalhaesandrade@gmail.com>
2023-09-20 13:48:29 +0100

    last updates

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,14 +1,11 @@
-import { Container, Image } from "react-bootstrap";
+import { Container, Image } from 'react-bootstrap';
 
 function HomePage() {
-
-
   return (
-    
-    <Container>
+    <Container style={{ marginBottom: '100px' }}>
       <h1>Old, but Gold!</h1>
       <h2>Astrolabe is your ultimate guide to your vintage purchases.</h2>
-      <Image src="src/assets/imagem-homepage.jpg" fluid/>
+      <Image src='src/assets/imagem-homepage.jpg' fluid />
     </Container>
   );
 }
--- src/pages/ProductDetailsPage/ProductDetailsPage.jsx
+++ src/pages/ProductDetailsPage/ProductDetailsPage.jsx
@@ -6,13 +6,14 @@ import { useParams } from 'react-router-dom';
 import CartContext from '../../contexts/CartContext';
 import { useContext } from 'react';
 import usersProducts from '../../constants/index';
+import { Link } from 'react-router-dom';
 
 function ProductDetailsPage() {
   const { id } = useParams();
   const { addToCart } = useContext(CartContext);
   const [result, setResult] = useState(null);
   const [loading, setLoading] = useState(true);
-  console.log(usersProducts);
+  const [userSellingProduct, setUserSellingProduct] = useState(null);
 
   useEffect(() => {
     axios
@@ -24,6 +25,26 @@ function ProductDetailsPage() {
       .catch(() => {
         setLoading(false);
       });
+
+    const userIndex = usersProducts.findIndex((user) =>
+      user.products.includes(parseInt(id))
+    );
+
+    if (userIndex !== -1) {
+      setUserSellingProduct(usersProducts[userIndex]);
+
+      axios
+        .get(`https://fakestoreapi.com/users/${userIndex + 1}`)
+        .then((res) => {
+          setUserSellingProduct((prevUser) => ({
+            ...prevUser,
+            userData: res.data,
+          }));
+        })
+        .catch((error) => {
+          console.error('Error fetching user data:', error);
+        });
+    }
   }, [id]);
 
   const handleAddToCart = () => {
@@ -31,7 +52,7 @@ function ProductDetailsPage() {
   };
 
   return (
-    <Container>
+    <Container style={{ marginBottom: '100px' }}>
       {loading ? (
         <Loading />
       ) : (
@@ -53,6 +74,19 @@ function ProductDetailsPage() {
               <br />
               <h1>{result.title}</h1>
               <p>{result.description}</p>
+              <p>
+                Sold by:{' '}
+                {userSellingProduct.userData ? (
+                  <Link
+                    className='text-dark'
+                    to={`/users/${userSellingProduct.userData.id}`}
+                  >
+                    {userSellingProduct.userData.username}
+                  </Link>
+                ) : (
+                  ''
+                )}
+              </p>
               <p>Price: {result.price}€</p>
 
               <Button className='mb-4' variant='dark' onClick={handleAddToCart}>
